aa538d5495f3aafd1c231ef2372ac3d744592472
Israel <israelromang@hotmail.com>
2022-04-29 16:56:23 -0500

    sacs.config: config file added
    input_output.py: Docstring updated
    
    sacs.py: Automatic evaluation of several lists and sorting algorithms
    
    sorting_algorithm.py: correction of the bubble sort algorithm

--- sacs.py
+++ sacs.py
@@ -14,11 +14,24 @@ import input_output as io
 
 
 def main():
-    unsorted_list = [89, 45, 68, 90, 29, 34, 17, 30]
-    quick_sorted_list = sa.quick_sort(unsorted_list)
-    #bubble_sorted_list = sa.bubble_sort(unsorted_list)
-    results = [("Quick sort", quick_sorted_list)]
-    io.formated_output(results)
+
+    args = io.parse_input()
+    config_file_path = args.input
+    output_file_path = args.output
+    config_dict = io.load_config_file(config_file_path)
+    results = []
+
+    sorting_algorithms = config_dict["sorting_algorithms"].split(",")
+    sorting_algorithms = [sorting_algorithm.strip() for sorting_algorithm in sorting_algorithms]
+    unsorted_lists = config_dict["unsorted_lists"].split("|")
+    unsorted_lists = [unsorted_list.strip("[] ").split(",") for unsorted_list in unsorted_lists]
+
+    for sorting_algorithm in sorting_algorithms:
+        for unsorted_list in unsorted_lists:
+            sorted_list = sa.sort(sorting_algorithm, unsorted_list)
+            results.append((sorting_algorithm, sorted_list))
+
+    io.formatted_output(results)
 
 
 if __name__ == "__main__":
--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -6,12 +6,21 @@ Functions:
     Quick sort: Sort an array of number, letters, and objects using the 
     quick sort strategy.
     Bubble sort: Sort an array of number, letters, and objects using the
-    buble sort strategy
+    bubble sort strategy
 
 """
 
 
-def quick_sort(unsorted_list : list) -> list:
+def sort(sorting_algorithm: str, unsorted_list: list) -> list:
+    sorted_list = None
+    if sorting_algorithm.lower() == "quick":
+        sorted_list = quick_sort(unsorted_list)
+    elif sorting_algorithm.lower() == "bubble":
+        sorted_list = bubble_sort(unsorted_list)
+    return sorted_list
+
+
+def quick_sort(unsorted_list: list) -> list:
     """Sort a list of numbers, letters, and objects using the brute force
     strategy called Quick sort
 
@@ -19,7 +28,7 @@ def quick_sort(unsorted_list : list) -> list:
 
     Parameters
     ----------
-    A : list
+    unsorted_list: list
         A is list with the elements to be sorted
 
     Returns
@@ -37,9 +46,6 @@ def quick_sort(unsorted_list : list) -> list:
             if sorted_list[j] < sorted_list[minimum]:
                 minimum = j
         swap_values(i, minimum, sorted_list)
-        #temp = sorted_list[i]
-        #sorted_list[i] = sorted_list[minimum]
-        #sorted_list[minimum] = temp
     return sorted_list
 
 
@@ -62,14 +68,16 @@ def bubble_sort(unsorted_list: list)->list:
     """
     
     sorted_list = unsorted_list.copy()
-    list_length = (unsorted_list)
-    for i in range(list_length - 2):
-        for j in range(list_length - 2 - i):
+    list_length = len(unsorted_list)
+    for i in range(list_length - 1):
+        for j in range(list_length - 1 - i):
             if sorted_list[j + 1] < sorted_list[j]:
                 swap_values(j, j + 1, sorted_list)
 
+    return sorted_list
 
-def swap_values(i:int, j:int, list_to_swap:list):
+
+def swap_values(i: int, j: int, list_to_swap: list):
     """ Swap the values of the index i and j in the list_to_swap list
 
         Parameters
